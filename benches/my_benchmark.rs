08fc38ee6529e9b9eb1709eb5b61785b153f70ee
Chris Eckhardt <eckhardt@bitwise.io>
2022-11-12 10:13:59 -0600

    Clean up code and benchmark

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,11 +1,12 @@
-
+#![allow(unused_imports)]
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use simd_examples::sum_up;
+use simd_examples::{sum_up, simd_sum_up};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("sum up", |b| b.iter(|| sum_up(black_box(1000000))));
+    c.bench_function("sum up", |b| {
+        b.iter(|| sum_up(black_box((0..=1000000).collect::<Vec<i64>>())))
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
 criterion_main!(benches);
-
--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,43 @@
+// Standard
 #[inline]
-pub fn sum_up(n: i64) -> i64 {
+pub fn sum_up(nums: Vec<i64>) -> i64 {
     // This structure matches how the SIMD function works and is not optimized away by the compiler
-    (0..=n).into_iter().fold(0, |a, x| sum(a, x))
+    nums.iter().fold(0, |a, x| regular_math(a, *x))
 }
 
-fn sum(a: i64, x: i64) -> i64 {
+fn regular_math(a: i64, x: i64) -> i64 {
+    // Normally, you'd do more complicated math than an addition - for example, calculate a
+    // squared error
     a + x
 }
 
+// SIMD
 use std::arch::x86_64::{_mm256_add_epi64, _mm256_set_epi64x};
 
 #[inline]
+pub fn simd_sum_up(nums: Vec<i64>) -> i64 {
+    // Iterate over 16 values at a time
+    // Each time we do `.map(|c| simd_math(c))`, it results in a vec that is 1/16 the length of the
+    // original.
+    // Chaining results in worse performance
+    nums.chunks(16).map(|c| simd_math(c)).sum::<i64>()
+}
+
 #[allow(unsafe_code)]
-pub fn sum_up_simd(slice: &[i64]) -> i64 {
+fn simd_math(slice: &[i64]) -> i64 {
     // SAFETY: I am running this on an x86_64 chip that I know has AVX2
     match slice {
-        // use simd if we have 16 values
-        // SAFETY: We know all of these values will be defined
+        // Use simd if we have 16 values
         &[v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16] => unsafe {
+            // SAFETY: We know all of these values will be defined
+            // Set four packed 64-bit vectors
             let vec1 = _mm256_set_epi64x(v1, v2, v3, v4);
             let vec2 = _mm256_set_epi64x(v5, v6, v7, v8);
             let vec3 = _mm256_set_epi64x(v9, v10, v11, v12);
             let vec4 = _mm256_set_epi64x(v13, v14, v15, v16);
 
+            // `transmute()` unpacks the 256-bit register into 4 i64 values
+            // `_mm256_add_epi64 performs the pair-wise sum across the vectors
             let (r1, r2, r3, r4): (i64, i64, i64, i64) = std::mem::transmute(_mm256_add_epi64(
                 _mm256_add_epi64(vec1, vec2),
                 _mm256_add_epi64(vec3, vec4),
@@ -41,18 +56,11 @@ mod tests {
 
     #[test]
     fn it_works() {
-        assert_eq!(sum_up(5), 15);
+        assert_eq!(sum_up((0..=5).collect::<Vec<i64>>()), 15);
     }
 
     #[test]
     fn simd_works() {
-        let mut nums: Vec<i64> = Vec::new();
-        for i in 1..=20 {
-            nums.push(i);
-        }
-        assert_eq!(
-            nums.chunks(16).map(|c| sum_up_simd(c)).sum::<i64>(),
-            210_i64
-        );
+        assert_eq!(simd_sum_up((0..=20).collect::<Vec<i64>>()), 210);
     }
 }
